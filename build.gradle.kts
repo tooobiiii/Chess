baac6101e00426e08829ee82aef14f173ee40edd
TobisuchtetXD <tobiasrothmeier1807@gmail.com>
2025-06-24 00:19:40 +0200

    fixed actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id("edu.sc.seis.launch4j") version "3.0.5"
 }
 
 group = "org.infoLK"
 version = "1.0-SNAPSHOT"
+val appMain = "org.infoLK.Main"
 
 repositories {
     mavenCentral()
@@ -12,4 +14,9 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.38")
     annotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+launch4j {
+    mainClassName = appMain
+    outfile = "ChessApp.exe"
 }
